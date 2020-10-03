ed26cae33db40ea73939bb30091879109e98965c
ArmoredThirteen <6456070+ArmoredThirteen@users.noreply.github.com>
2020-10-02 17:00:08 -0700

    Changed bool toggle for mod effect to be a ModExtent enum to control if it modifies only the gear it is in, the compartment, or the entire golem

--- Golmch/Assets/Scripts/Abilities/Ability_Modifier.cs
+++ Golmch/Assets/Scripts/Abilities/Ability_Modifier.cs
@@ -6,9 +6,7 @@ namespace ATE
 {
     public abstract class Ability_Modifier : Ability
 	{
-        // If true, only applies to gears within the same compartment
-        // If false, applies to all gears within the golem
-        public bool compartmentOnly = true;
+        public ModExtent extent = ModExtent.Compartment;
 
         public List<GearType> allowedGearTypes = new List<GearType> ();
 	}
