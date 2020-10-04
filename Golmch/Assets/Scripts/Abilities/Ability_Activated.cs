c8f26ece81f2f69ab5d87c6df4debcaf8fcfcc77
ArmoredThirteen <6456070+ArmoredThirteen@users.noreply.github.com>
2020-10-03 17:48:47 -0700

    Added targeting and area types

--- Golmch/Assets/Scripts/Abilities/Ability_Activated.cs
+++ Golmch/Assets/Scripts/Abilities/Ability_Activated.cs
@@ -6,6 +6,10 @@ namespace ATE
 {
     public abstract class Ability_Activated : Ability
 	{
+        public TargetingType targetingType = TargetingType.Entity;
+        public float maxTargetDistance = 1;
+        public AreaType areaType = AreaType.Single;
+
         public abstract void Activate(GameEntity source, params GameEntity[] targets);
 		
 	}
---
+++ Golmch/Assets/Scripts/Targeting/AreaType.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ATE
+{
+	public enum AreaType
+	{
+		Single = 0,
+        Ray = 10,
+        Cone = 20,
+        Radius = 30,
+	}
+}
